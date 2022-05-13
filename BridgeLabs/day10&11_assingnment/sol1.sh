198eceee82dd95d8a3521544f1bb8cf3071ef408
NitinNKS <nitinkrishan1995@gmail.com>
2022-05-13 19:48:33 +0530

    Fifth Commit

---
+++ BridgeLabs/day10&11_assingnment/sol1.sh
@@ -0,0 +1,78 @@
+
+
+read -p "Enter any  three numbers in same row followed by space  : " a b c
+echo $a $b $c 
+
+compute1=$((a+b*c))
+echo $compute1
+
+compute2=$((a*b+c))
+echo $compute2
+
+compute3=$( echo "scale=0;" $c+$a/$b | bc -l )
+#printf '%0.2f' "$compute3"
+echo $compute3
+
+compute4=$( echo "scale=0;" $a%$b+$c | bc -l )
+#printf '%0.2f' "$compute4"
+echo $compute4
+
+
+
+declare -A dict
+
+dict[0]=compute1
+dict[1]=compute2
+dict[2]=compute3
+dict[3]=compute4
+
+
+declare -a arr=( $compute1 $compute2 $compute3 $compute4 )
+
+
+
+
+echo "Array in original order"
+echo ${arr[*]}
+
+# Performing Bubble sort in ascending order
+for ((i = 0; i<4; i++))
+do
+    
+    for((j = 0; j<4-i-1; j++))
+    do
+    
+        if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
+        then
+            # swap
+            temp=${arr[j]}
+            arr[$j]=${arr[$((j+1))]}  
+            arr[$((j+1))]=$temp
+        fi
+    done
+done
+
+echo "Array in sorted order , Ascending  :"
+echo ${arr[*]}
+
+
+
+# Performing Bubble sort in descending order
+for ((i = 0; i<4; i++))
+do
+
+    for((j = 0; j<4-i-1; j++))
+    do
+
+        if [ ${arr[j]} -lt ${arr[$((j+1))]} ]
+        then
+            # swap
+            temp=${arr[j]}
+            arr[$j]=${arr[$((j+1))]}
+            arr[$((j+1))]=$temp
+        fi
+    done
+done
+
+echo "Array in sorted order , Descending  :"
+echo ${arr[*]}
